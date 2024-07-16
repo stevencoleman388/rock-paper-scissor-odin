c18375c499c92e2dc7cbc2638e13eb8136664d8a
Steven Coleman <stevencoleman388@gmail.com>
2024-07-16 16:07:41 -0700

    I deleted any unessary code,comments and added some information to be able to play

--- scripts.js
+++ scripts.js
@@ -1,24 +1,8 @@
-/* create a function that will randomly return “rock”, “paper” or “scissors” for computer 
-getComputerChoice()
-*/
-/*Create function for humans choice 
-getHumanChoice()
-*/
-/*create variable to keep count of humanScore and computerScore  */
-/*
-Write logic to play a single round 
-create two variables and assign humanChoice to getHumanChoice() and computerChoice to getComputerChoice() in playRound
-playRound(humanChoice,computerChoice)
-*/
-
-/* Create a new function named playGame().
-Move your playRound function and score variables so that they’re declared inside of the new playGame function
-Play 5 rounds by calling playRound 5 times. */
-let btn = document.querySelector("button");
 let computerChoice;
 function getComputerChoice() {
+  /* Math.floor(Math.random()*3) returns wither 0,1,2  */
   let computerNumber = Math.floor(Math.random() * 3);
-
+  /*Assiging a value to each result from above. */
   if (computerNumber == 0) {
     computerChoice = "rock";
   } else if (computerNumber == 1) {
@@ -26,10 +10,9 @@ function getComputerChoice() {
   } else if (computerNumber == 2) {
     computerChoice = "scissors";
   }
-
   return computerChoice;
 }
-let count = 0;
+
 let usersChoice;
 function getHumanChoice() {
   usersChoice = prompt("Rock, Paper or Scissors").toLowerCase();
@@ -39,25 +22,15 @@ function getHumanChoice() {
     usersChoice != "scissors"
   ) {
     console.log("invalid");
-  } else {
   }
-
   return usersChoice;
 }
-
-/*
-Function to for functionality to compare to each choice humanSelection=getHumanChoice() and computerSelection=getComputerChoice()
-playRound(humanChoice, computerChoice); */
-
-// let humanSelection = getHumanChoice();
-// let computerSelection = getComputerChoice();
-// playRound(humanSelection, computerSelection);
 
 function playGame() {
   let count = 0;
   let computerScore = 0;
   let playerScore = 0;
-  // let score = ;
+
   function playRound(humanChoice, computerChoice) {
     if (humanChoice == "rock" && computerChoice == "scissors") {
       playerScore++;
@@ -74,32 +47,14 @@ function playGame() {
     } else if (humanChoice == computerChoice) {
       console.log("tie");
     }
-    // count++;
   }
-
-  // playRound(humanSelection, computerSelection);
-
-  // getComputerChoice();
-  // console.log(humanSelection, playerScore);
-  // console.log(computerSelection, computerScore);
-  // console.log(count);
   while (count < 5) {
     playRound(getHumanChoice(), getComputerChoice());
     count++;
     console.log(`Player chooses ${usersChoice} score ${playerScore}`);
     console.log(`Computer chooses ${computerChoice} score ${computerScore}`);
-
     console.log(count);
   }
-  // console.log(humanSelection, playerScore);
-  // console.log(computerSelection, computerScore);
-  // return playerScore;
   console.log(`Final Score Player ${playerScore}, Computer ${computerScore}`);
 }
 playGame();
-// btn.addEventListener("click", playGame);
-//  getHumanChoice();
-//  getComputerChoice();
-//  console.log(playerScore);
-//  console.log(getHumanChoice());
-//  console.log(getComputerChoice());
